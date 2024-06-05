c13520400df892b0e7fc44da654d8a8a7c0c63a8
Abhaya G <gautamabhaya98@gmail.com>
2024-06-06 01:23:45 +0545

    Update: Customer  model added

--- photographer_booking_app/app/models.py
+++ photographer_booking_app/app/models.py
@@ -4,9 +4,13 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 from config.settings.base import MEDIA_URL
 
-"""Photographer model defines a photographer entity"""
+
 
 class Photographer(models.Model):
+
+    """Photographer model defines a photographer entity"""
+
+
     first_name = models.CharField(max_length=250, blank=False)
     last_name = models.CharField(max_length=250, blank=False)
     address = models.CharField(max_length=300)
@@ -28,3 +32,22 @@ class Photographer(models.Model):
     def __str__(self):
         """String for representing the model object."""
         return f'{self.first_name} {self.last_name}' 
+    
+
+class Customer(models.Model):
+
+    """Customer model defines a customer entity""" 
+
+
+    first_name = models.CharField(max_length=250, blank=False)
+    last_name = models.CharField(max_length=250, blank=False)
+    address = models.CharField(max_length=300)
+    email = models.EmailField(max_length=254, blank=False, null=False)
+    Phone_Number = PhoneNumberField(max_length = 15, null=False, blank=False, verbose_name='Phone')
+
+
+    def __str__(self):
+        """String for representing the model object."""
+        return f'{self.first_name} {self.last_name}' 
+    
+
