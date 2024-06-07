6660291e2886fdbdef7f0defd152be262d575ae2
Abhaya G <gautamabhaya98@gmail.com>
2024-06-07 12:31:05 +0545

    Update: Customer model removed from photographer_booking_app.app

--- photographer_booking_app/app/models.py
+++ photographer_booking_app/app/models.py
@@ -63,20 +63,5 @@ class Photographer(models.Model):
         return f'{self.first_name} {self.last_name}' 
     
 
-class Customer(models.Model):
-
-    """Customer model defines a customer entity""" 
-
-
-    first_name = models.CharField(max_length=250, blank=False)
-    last_name = models.CharField(max_length=250, blank=False)
-    address = models.CharField(max_length=300)
-    email = models.EmailField(max_length=254, blank=False, null=False)
-    Phone_Number = PhoneNumberField(max_length = 15, null=False, blank=False, verbose_name='Phone')
-
-
-    def __str__(self):
-        """String for representing the model object."""
-        return f'{self.first_name} {self.last_name}' 
     
 
