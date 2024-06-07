28a733b59bb62fc6adca5709a128c1d72223ce10
Abhaya G <gautamabhaya98@gmail.com>
2024-06-07 12:38:07 +0545

    Update: Customer model removed from photographer_booking_app.app

--- booking/models.py
+++ booking/models.py
@@ -7,7 +7,7 @@ import uuid
 # Create your models here.
 
 class BookBaseModel(models.Model):
-    Ticket = models.UUIDField
+    Ticket = models.UUIDField(uuid)
     
     
 
@@ -17,7 +17,7 @@ class BookCall(BookBaseModel):
     Booking_Date_Time = models.DateTimeField()
     
 
-class BookaSession(BookBaseModel):
+class BookASession(BookBaseModel):
     Customer = models.OneToOneField(Customer, null=False, blank=False)
     Photographer = models.OneToOneField(Photographer, null=False, blank=False)
     Booking_Date_Time = models.DateTimeField()
