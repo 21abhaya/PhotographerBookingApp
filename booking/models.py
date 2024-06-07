2714ac291c8e056fa65b484121b289402692a006
Abhaya G <gautamabhaya98@gmail.com>
2024-06-07 12:28:28 +0545

    Update: Customer and Booking models updated.

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from customer.models import Customer
+from photographer_booking_app.app.models import Photographer
+
+import uuid
 
 # Create your models here.
+
+class BookBaseModel(models.Model):
+    Ticket = models.UUIDField
+    
+    
+
+class BookCall(BookBaseModel):
+    Customer = models.OneToOneField(Customer, null=False, blank=False)
+    Photographer = models.OneToOneField(Photographer, null=False, blank=False)
+    Booking_Date_Time = models.DateTimeField()
+    
+
+class BookaSession(BookBaseModel):
+    Customer = models.OneToOneField(Customer, null=False, blank=False)
+    Photographer = models.OneToOneField(Photographer, null=False, blank=False)
+    Booking_Date_Time = models.DateTimeField()
+     
+        
+    
