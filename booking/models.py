73b5fb9ab1c45811f9d7187aafd4a2fc95d32421
Abhaya G <gautamabhaya98@gmail.com>
2024-06-08 23:43:00 +0545

    Update: added return string and reverse url methods for booking models.

--- booking/models.py
+++ booking/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 from customer.models import Customer
 from photographer.models import Photographer
@@ -21,12 +22,22 @@ class BookCall(BookBaseModel):
     photographer = models.OneToOneField(Photographer, null=False, blank=False, on_delete=models.RESTRICT)
     booked_for = models.DateTimeField()
     
+    def __str__(self):
+        return f"Call Booked by{self.customer}"
+    
+    def get_absolute_url(self):
+        return reverse('call-book-detail', args=[str(self.id)])
 
 class BookASession(BookBaseModel):
     customer = models.OneToOneField(Customer, null=False, blank=False, on_delete=models.RESTRICT)
     photographer = models.OneToOneField(Photographer, null=False, blank=False, on_delete=models.RESTRICT)
     booked_For = models.DateTimeField()
 
+    def __str__(self):
+        return f"Session Booked by{self.customer}"
+
      
+    def get_absolute_url(self):
+        return reverse('session-book-detail', args=[str(self.id)])
         
     
