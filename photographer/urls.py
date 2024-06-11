1658f6ad2258511bf01da09f6be411864f9732bd
Abhaya G <gautamabhaya98@gmail.com>
2024-06-12 00:13:27 +0545

    Added: CRUD views for Photographer.

--- photographer/urls.py
+++ photographer/urls.py
@@ -4,9 +4,9 @@ from .views import *
 app_name = "photographer"
 
 urlpatterns =[
-    path('photographer-create/', views.PhotographerCreateView.asView, name='photographer-create'),
-    path('photographer-list/', views.PhotographerListView.asView, name='photographers'),
-    path('photographer-detail/<str:pk>', views.PhotographerDetailView.asView, name='photographer-detail'),
-    path('photographer-update/<str:pk>', views.PhotographerUpdateView.asView, name='photographer-update'),
-    path('photographer-delete/<str:pk>', views.PhotographerdeleteView.asView, name='photographer-delete'),
+    path('photographer-create/', PhotographerCreateView.asView, name='photographer-create'),
+    path('photographers/', PhotographerListView.asView, name='photographers-list'),
+    path('photographer-detail/<str:pk>', PhotographerDetailView.asView, name='photographer-detail'),
+    path('photographer-update/<str:pk>', PhotographerUpdateView.asView, name='photographer-update'),
+    path('photographer-delete/<str:pk>', PhotographerDeleteView.asView, name='photographer-delete'),
 ]
--- photographer/views.py
+++ photographer/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import CreateView,ListView,DetailView,UpdateView,DeleteView
+
+from .models import Photographer
 
 # Create your views here.
 
+class PhotographerCreateView(CreateView):
+    model = Photographer
+    # form_class =
+    template_name = 'photographer_create_update_view.html'
+    success_url = reverse_lazy('photographer:photographers-list')
+
+class PhotographerListView(ListView):
+    model = Photographer
+    # form_class = 
+    template_name = 'photographer_list_view.html'
+    context_object_name = 'photographers'
+    paginate_by = 10
+
+class PhotographerDetailView(DetailView):
+    model = Photographer
+    template_name = 'photographer_detail_view.html'
+    
+    
+class PhotographerUpdateView(CreateView):
+    model = Photographer
+    # form_class =
+    template_name = 'photographer_delete_view.html'
+    success_url = reverse_lazy('photographer:photographers-list')
+
+class PhotographerDeleteView(DeleteView):
+    model = Photographer
+    template_name = 'photographer_delete_view.html'
+    success_url = reverse_lazy('photographer:photographers-list')
