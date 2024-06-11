66170a9e7cf64e88d541959e4407a2ec80166ae2
Abhaya G <gautamabhaya98@gmail.com>
2024-06-12 00:28:40 +0545

    Added: CRUD views for Customer.

--- customer/urls.py
+++ customer/urls.py
@@ -5,8 +5,8 @@ app_name = "customer"
 
 urlpatterns =[
     path('customer-create/', CustomerCreateView.asView, name='customer-create'),
-    path('customers/', CustomerListView.asView, name='customer-create'),
-    path('customer-detail/<str:pk>', CustomerDetailView.asView, name='customer-create'),
-    path('customer-update/<str:pk>', CustomerUpdateView.asView, name='customer-create'),
-    path('customer-delete/<str:pk>', CustomerDeleteView.asView, name='customer-create'),   
+    path('customers/', CustomerListView.asView, name='customer-list'),
+    path('customer-detail/<str:pk>', CustomerDetailView.asView, name='customer-detail'),
+    path('customer-update/<str:pk>', CustomerUpdateView.asView, name='customer-update'),
+    path('customer-delete/<str:pk>', CustomerDeleteView.asView, name='customer-delete'),   
 ]
