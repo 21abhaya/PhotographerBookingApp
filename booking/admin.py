3d1e38fe424e8ad61952aae925b66ae23af5bfb2
Abhaya G <gautamabhaya98@gmail.com>
2024-06-12 23:04:53 +0545

    Update: All models registered to the admin.

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from booking.models import BookBaseModel, BookCall, BookASession
 # Register your models here.
+
+admin.site.register(BookBaseModel)
+admin.site.register(BookCall)
+admin.site.register(BookASession)
--- photographer/admin.py
+++ photographer/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from photographer.models import Photographer, Portfolio
 
 # Register your models here.
+admin.site.register(Photographer)
+admin.site.register(Portfolio)
