7d3cc2b85ef7f934894ce1c504d1db1858d06b9f
Abhaya G <gautamabhaya98@gmail.com>
2024-06-18 00:08:28 +0545

    Added test_func, dispatch and handle_no_perms to customer views.

--- customer/views.py
+++ customer/views.py
@@ -39,10 +39,46 @@ class CustomerListView(ListView, UserPassesTestMixin, PermissionRequiredMixin, L
     template_name = 'customer_list_view.html'
     paginate_by = 10
 
+    def test_func(self):
+        return self.request.user.is_staff
+    
+    def handle_no_permission(self):
+        if self.request.user.is_authenticated:
+            messages.error(
+                self.request,
+                "You do not have permission to view customers' list.",
+            )
+            return render(self.request, HttpResponseRedirect)
+        return super().handle_no_permission()
+    
+    def dispatch(self, request, *args, **kwargs):
+        if not self.test_func():
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
+
+
 class CustomerDetailView(DetailView, UserPassesTestMixin, PermissionRequiredMixin, LoginRequiredMixin):
     model = Customer
     context_object_name = 'customer'
     template_name = 'customer_detail_view.html'
+
+    def test_func(self):
+        return self.request.user.is_staff
+    
+    def handle_no_permission(self):
+        if self.request.user.is_authenticated:
+            messages.error(
+                self.request,
+                "You do not have permission to view customer details.",
+            )
+            return render(self.request, HttpResponseRedirect)
+        return super().handle_no_permission()
+    
+    def dispatch(self, request, *args, **kwargs):
+        if not self.test_func():
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
+
 
     
 class CustomerUpdateView(UpdateView, UserPassesTestMixin, PermissionRequiredMixin, LoginRequiredMixin):
@@ -51,7 +87,45 @@ class CustomerUpdateView(UpdateView, UserPassesTestMixin, PermissionRequiredMixi
     template_name = 'customer_create_update_view.html'
     success_url = reverse_lazy('customer:customers-list')
 
+    def test_func(self):
+        return self.request.user.is_staff or self.request.user.has_perm('customer.update_customer')
+    
+    def handle_no_permission(self):
+        if self.request.user.is_authenticated:
+            messages.error(
+                self.request,
+                "You do not have permission to update customer information.",
+            )
+            return render(self.request, HttpResponseRedirect)
+        return super().handle_no_permission()
+    
+    def dispatch(self, request, *args, **kwargs):
+        if not self.test_func():
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
+
+
 class CustomerDeleteView(DeleteView, UserPassesTestMixin, PermissionRequiredMixin, LoginRequiredMixin):
     model = Customer
     template_name = 'customer_delete_view.html'
     success_url = reverse_lazy('customer:customers-list')
+
+    def test_func(self):
+        return self.request.user.is_staff or self.request.user.has_perm('customer.delete_customer')
+    
+    def handle_no_permission(self):
+        if self.request.user.is_authenticated:
+            messages.error(
+                self.request,
+                "You do not have permission to delete customer information.",
+            )
+            return render(self.request, HttpResponseRedirect)
+        return super().handle_no_permission()
+    
+    def dispatch(self, request, *args, **kwargs):
+        if not self.test_func():
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
+
+
+    
